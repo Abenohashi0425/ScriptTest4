f183c5013438c2b838b66fac2d9caf477b5a73ec
naotada.abe <kawagoe201187@gmail.com>
2019-06-06 06:58:51 +0900

    ScriptTest4プロジェクト　初めてのコミット
    課題提出用のスクリプト言語を記載したファイルです。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test: MonoBehaviour
+public class Test : MonoBehaviour
 {
 
     // Use this for initialization
@@ -22,34 +22,15 @@ public class Test: MonoBehaviour
         {
             Debug.Log(points[i]);
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        //Debug.Log(points[4]);
+        //Debug.Log(points[3]);
+        //Debug.Log(points[2]);
+        //Debug.Log(points[1]);
+        //Debug.Log(points[0]);
 
-    }
-}
-
-
-
-
-
-    // Use this for initialization
-    void Start()
-    {
-        // 要素数5の配列を初期化する
-        int[] points = new int[5];
-
-        // 配列の各要素に値を代入する
-        points[0] = 400;
-        points[1] = 300;
-        points[2] = 200;
-        points[3] = 100;
-        points[4] = 50;
-
-        // 配列の要素をすべて表示する
-        for (int i = 0; i < 5; i++)
+        // 逆を表示
+        for (int i = 4; i >= 0; i--)
         {
             Debug.Log(points[i]);
         }
